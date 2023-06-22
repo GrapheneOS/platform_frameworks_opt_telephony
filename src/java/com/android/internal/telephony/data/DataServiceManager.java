659dca558c8d910ca626df3ede85721d7544780b
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-06-22 11:24:19 +0300

    ignore non-system data service packages
    src/java/com/android/internal/telephony/dataconnection/DataServiceManager.java no longer exists in 14 and was skipped
    
    Change-Id: Ie84e37ff088f6b569f3a9d68cb42c7160610b7a8

--- src/java/com/android/internal/telephony/data/DataServiceManager.java
+++ src/java/com/android/internal/telephony/data/DataServiceManager.java
@@ -55,6 +55,7 @@ import android.telephony.data.IDataServiceCallback;
 import android.telephony.data.NetworkSliceInfo;
 import android.telephony.data.TrafficDescriptor;
 import android.text.TextUtils;
+import android.util.PackageUtils;
 import android.util.Pair;
 
 import com.android.internal.telephony.IIntegerConsumer;
@@ -592,6 +593,10 @@ public class DataServiceManager extends Handler {
         if (!b.isEmpty() && !TextUtils.isEmpty(b.getString(carrierConfig))) {
             // If carrier config overrides it, use the one from carrier config
             packageName = b.getString(carrierConfig, packageName);
+        }
+
+        if (!PackageUtils.isSystemPackage(mPhone.getContext(), packageName)) {
+            return "";
         }
 
         return packageName;
