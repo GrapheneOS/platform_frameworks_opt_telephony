b4f3d8230bb196551fa2944d89b880f8aaf726d9
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2024-05-30 16:48:15 +0300

    allow SYSTEM_UID to call EuiccCardController.resetMemory()
    Before this commit, only eUICC LPA was allowed to call that method.
    
    resetMemory() is now used in system_server to wipe eUICCs as part of the duress password feature
    implementation.

--- src/java/com/android/internal/telephony/euicc/EuiccCardController.java
+++ src/java/com/android/internal/telephony/euicc/EuiccCardController.java
@@ -32,6 +32,7 @@ import android.content.pm.ComponentInfo;
 import android.content.pm.PackageManager;
 import android.os.Binder;
 import android.os.Handler;
+import android.os.Process;
 import android.os.RemoteException;
 import android.preference.PreferenceManager;
 import android.provider.Settings;
@@ -703,15 +704,17 @@ public class EuiccCardController extends IEuiccCardController.Stub {
     @Override
     public void resetMemory(String callingPackage, String cardId,
             @EuiccCardManager.ResetOption int options, IResetMemoryCallback callback) {
-        try {
-            checkCallingPackage(callingPackage);
-        } catch (SecurityException se) {
+        if (Binder.getCallingUid() != Process.SYSTEM_UID) {
             try {
-                callback.onComplete(EuiccCardManager.RESULT_CALLER_NOT_ALLOWED);
-            } catch (RemoteException re) {
-                loge("callback onComplete failure after checkCallingPackage.", re);
+                checkCallingPackage(callingPackage);
+            } catch (SecurityException se) {
+                try {
+                    callback.onComplete(EuiccCardManager.RESULT_CALLER_NOT_ALLOWED);
+                } catch (RemoteException re) {
+                    loge("callback onComplete failure after checkCallingPackage.", re);
+                }
+                return;
             }
-            return;
         }
 
         enforceTelephonyFeatureWithException(callingPackage, "resetMemory");
